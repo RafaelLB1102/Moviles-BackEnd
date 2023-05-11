fab1d6cf36789c330852b1db3e4bfb63ca8656c8
Rafael Londoño <athenapms12345@gmail.com>
2023-05-11 17:53:06 -0500

    first commit

---
+++ constants.js
@@ -0,0 +1,19 @@
+const DB_HOST = "db-moviles.mlbf2qd.mongodb.net";
+const DB_USER = "user-moviles";
+const DB_PASS = "6nsW6CciLr7RdIf8";
+const DB_NAME = "DB-MOVILES";
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+const JWT_SECRET_KEY = "acceso-jwt";
+
+
+
+module.exports = {
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY,
+}
